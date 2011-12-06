4f4e900119bb18abddc50bf5400d5c8af131ac0c
yowana <y.kuvaka.09@aberdeen.ac.uk>
2011-12-06 14:37:24 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photos::Application.config.secret_token = 'd394c3261922fd2baf8a054edad5bb42561af9df98d7f44f0ae701b59bc73ba575c4ecdd1668d6fbd6a59d36439939ca07169df9ea83a0cd778f54a079801772'
